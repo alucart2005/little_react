a4cc1ca33d3ddd01a31babb22259afa760acc779
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-08-30 17:43:37 -0500

    feat: autocomplete of components

--- src/components/Coffe.jsx
+++ src/components/Coffe.jsx
@@ -1,6 +1,16 @@
-function Coffe() {
-  const hot=false;
-  return (<h1>{hot ? "Hot Coffe" : "Cold Coffe"}</h1>)
+export function Coffe() {
+  const data = {
+    name: "Hot Coffe",
+    price: 100,
+    id: 1,
+  };
+
+  return <div>
+    <h1>{data.name}</h1>
+    <h2>{data.price}</h2>
+    <h3>{data.id}</h3>
+  </div>;
+  // const hot=false;
+  // return (<h1>{hot ? "Hot Coffe" : "Cold Coffe"}</h1>)
 }
 
-export default Coffe;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Coffe from './components/Coffe';
+import { Coffe } from './components/Coffe';
+import Users from './components/Users';
+//import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Coffe />
-    <App />
+    <Users />
   </React.StrictMode>
 );
 
