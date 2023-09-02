9055dcb07687c0874c8ae4da095ee6d8898bd596
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-09-01 23:08:06 -0500

    feat: Hooks 3

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -4,15 +4,21 @@ import { useState } from "react";
 
 export function Test() {
   const [likes, setLikes] = useState(100);
+  const [theme, setTheme] = useState(false);
   const giveLikes = () => {
     setLikes(likes + 1);
   };
   const unLikes = () => {
     setLikes(likes - 1);
   };
+  const changeTheme = () => {
+    setTheme(!theme);
+    console.log(theme);
+  };
   return (
-    <div className="card">
+    <div className={"card" + " " + (theme ? "on" : "off")}>
       <Title>Likes {likes}</Title>
+      <button onClick={changeTheme}>Change Theme</button>
       <button onClick={giveLikes}>Give Likes</button>
       <button onClick={unLikes}>UnLike</button>
     </div>
